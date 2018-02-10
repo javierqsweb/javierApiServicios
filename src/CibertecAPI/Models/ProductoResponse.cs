96955055eabd710d344d2b4df747305d4b032e0e
Your Name <you@example.com>
2018-02-10 18:02:29 -0500

    first commit

---
+++ src/CibertecAPI/Models/ProductoResponse.cs
@@ -0,0 +1,13 @@
+﻿using Cibertec.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CibertecAPI.Models
+{
+    public class ProductoResponse
+    {
+        public IEnumerable<Producto> Items { get; set; }
+    }
+}
